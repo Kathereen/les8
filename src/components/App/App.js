93fde4f2dc5b93df92f3295e620879fc2b41f6f7
Kathereen <duodektill@ukr.net>
2021-12-24 03:39:16 +0200

    NewUpdates

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,122 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { renderRoutes } from 'react-router-config';
+import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
 import Page1 from "../Page1";
 import Page2 from "../Page2";
 import HomePage from '../HomePage';
 import './App.css';
 
+
+const Main = ({ route }) =>{
+  return( 
+  <div>
+      {renderRoutes(route.routesConf)}
+  </div>
+  )}
+
+const Food = () => {
+  return(<h3>Food</h3>);
+}
+
+const Goods = (props) =>{
+  return(
+  <div>
+      <h3>Goods</h3>
+      {renderRoutes(props.route.routesConf)(props ='hello')}
+  </div>
+  )}
+
+const HomeGoods = (props) =>(
+  <div>
+    <h4>HomeGoods</h4>
+    {props}
+  </div>
+  ) 
+const SportGoods = (props) =>(
+  <div>
+    <h4>SportGoods</h4>
+    <span>{props}</span>
+  </div>
+  )
+
+const routesConf = [
+  {
+    component: Main,
+    routes:[
+    {
+        path:'/food',
+        component: Food,
+    },
+      {
+        path:'/goods/:id',
+        component: Goods,
+        routes:[
+          {
+            path: '/goods/:id/home-goods',
+            component: HomeGoods
+          },
+          {
+            path: '/goods/:id/sport-goods',
+            component: SportGoods
+          }
+        ]
+      }
+    ]
+
+  }  
+]
+
 class App extends React.Component {
   render(){
   return (
-    <div className="App"> App
+    <div className="App"> 
       <Router>
-  {/* <Switch> */}
-      <Route path='/'exact component={HomePage} />
+        <menu>
+          {/* TASK__1 */}
+              <ul>
+                <li><Link to='/food'>Food</Link></li>
+                <li><Link to='/goods'>Goods</Link>
+                  <ol>
+                    <li>
+                      <Link to='/home-goods'>HomeGoods</Link>
+                    </li>
+                    <li>
+                      <Link to='/sport-goods'>SportGoods</Link>
+                    </li>
+                  </ol>
+                </li>
+              </ul>
+
+{/* TASK__2 */}
+          {/* <li>
+            <Link to='/home'>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to='/page1' >
+              page 1
+            </Link>
+          </li>
+          <li>
+            <Link to='/page2' >
+              page 2
+            </Link> 
+          </li>   */}
+        </menu>
+
+{/* TASK__2 */}
+
+  {/* <Switch>
+      <Route path='/home'exact component={HomePage} />
       <Route path='/page2'  component={Page2}/>
       <Route path='/page1'  component={Page1}/>
-  {/* </Switch>     */}
-      </Router>
-      
+  </Switch>     */}
+
+{/* TASK__1 */}
+
+      {renderRoutes(routesConf)}
+      </Router>  
     </div>
   )}
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { renderRoutes} from 'react-router-config';
 import './index.css';
+import {routesConf}  from './components/App/App';
 import App from './components/App/App';
 import { Router } from "react-router-dom";
 import {createBrowserHistory} from 'history';
@@ -10,6 +12,8 @@ const history = createBrowserHistory()
   ReactDOM.render((
      <Router history={history}>
        <App/>
+       {renderRoutes(routesConf)}
      </Router>
    ), document.getElementById('root')
 );
+
